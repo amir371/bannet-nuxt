96123d9303bd5d537c8323bdbb4703b78ad211e0
amir <amir.irany1404@yahoo.com>
2021-04-09 01:08:21 +0430

    pagelogin

---
+++ pages/about.vue
@@ -0,0 +1,132 @@
+<template>
+<div>
+  <img  class="back" src="@/assets/img/arrowleft.png" alt="logoEmbajador">
+  <div class="top">
+    <h1>BANNET</h1>
+    <p>فروشگاه هوشمند بنت</p>
+  </div>
+  <p class="center">کد ارسالی وارد نمایید</p>
+  <p class="title5ragham">کد5رقمی به شماره شما پیامک شد</p>
+  <div class="phone">
+    <img src="@/assets/img/edit.png" alt="">
+    <p>09370402434</p>
+  </div>
+  <div class="number5ragham">
+    <p>2</p>
+    <p>6</p>
+    <p>9</p>
+    <p>4</p>
+    <p>5</p>
+  </div>
+  <div class="titlesunder">
+    <div class="mojadadpayamak">ارسال مجدد پیامک</div>
+    <div class="dontpayamak"> پیامکی دریافت نکرده اید؟</div>
+  </div>
+  <div class="button">
+    <p>ارسال</p>
+  </div>
+</div>
+
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss" scoped>
+body{
+  position: relative;
+}
+.back{
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding: 10px;
+  color: blueviolet;
+}
+.top {
+  margin-top: 10rem;
+  text-align: center;
+}
+.top h1{
+  font-size: 30px;
+  color: $third_text_color;
+}
+.top p{
+  color: blueviolet;
+}
+.center {
+  margin-top: 100px;
+  text-align: center;
+  font-size: 20px;
+  color: $third_text_color;
+}
+.title5ragham{
+  padding: 10px;
+  color:#cdcdcd;
+  text-align: center;
+  font-size: 12px;
+}
+
+.phone{
+  display: flex;
+  border-radius: 20px;
+  justify-content: center;
+  background: rgba(137, 43, 226, 0.151);
+  width: 35%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 5px;
+}
+.phone img{
+  width: 20px;
+  height: 20px;
+}
+.phone p{
+   color: rgb(137, 43, 226);
+}
+.number5ragham{
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+.number5ragham p{
+  margin-right: 5px;
+   background: #F5F5F5;
+   border-radius: 15px;
+   width: 50px;
+   height: 50px;
+   color: $third_text_color;
+   text-align: center;
+   font-size: 30px;
+}
+.titlesunder{
+  display: flex;
+  justify-content: space-between;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  margin-top: 10px;
+  font-size: 10px;
+}
+.dontpayamak{
+  color: $third_text_color;
+}
+.mojadadpayamak{
+  color: blueviolet;
+}
+
+.button{
+  position: fixed;
+  bottom: 10px;
+  right: 0;
+  left: 0;
+  margin-right: 20px;
+  margin-left: 20px;
+  border: 2px solid blueviolet;
+  border-radius: 15px;
+  background-color: blueviolet;
+  color: white;
+  text-align: center;
+  padding: 10px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,85 @@
 <template>
-  <div>Hello World</div>
-</template>
+<div>
+  <img  class="back" src="@/assets/img/arrowleft.png" alt="logoEmbajador">
+  <div class="center">
+    <h1>BANNET</h1>
+    <p>فروشگاه هوشمند بنت</p>
+  </div>
+  <div class="inputnumber">
+    <p>:شماره موبایل</p>
+    <div >
+      <input type="text" placeholder="091******">
 
+    </div>
+  </div>
+  <p class="ghanin">مطالعه قوانین و مقررات</p>
+  <div class="button">
+    <NuxtLink to="/about">دریافت کد</NuxtLink>
+  </div>
+</div>
+
+</template>
 <script>
 export default {
-  data: () => ({}),
-  head: () => ({
-    title: 'Bannet | Dashboard',
-    meta: [
-      // SEO Examples
-      { hid: 'description', name: 'description', content: 'Gemnote VueJS Frontend Assessment' },
-      { hid: 'keywords', name: 'keywords', content: 'Gemnote, swag and gifts, gifts, gift inventory' },
-      { name: 'robots', content: 'noindex, nofollow' },
-      // Social Media
-      { property: 'og:site_name', content: 'Gemnote' },
-      { property: 'og:title', content: 'Gemnote VueJS Frontend Assessment' },
-      { property: 'og:description', content: 'Gemnote by VueJs | NuxtJS' },
-      { property: 'og:image', content: 'https://assets.website-files.com/5c7449f43783556baa54bd0e/60540cde101f8399aa9f3a8d_Gemnote_WebCover.png' },
-      { property: 'og:url', content: 'https://gemnote.com' },
-      { property: 'og:url', content: 'https://gemnote.com' },
-      { name: 'twitter:card', content: 'twitterCard' },
-      { name: 'twitter:image:alt', content: 'Alt text for image' },
-      { name: 'twitter:site', content: '@website-username' },
-      { name: 'fb:app_id', content: 'your_app_id' }
-    ]
-  })
 }
 </script>
-
-<style lang="scss">
-
+<style lang="scss" scoped>
+body{
+  position: relative;
+}
+.back{
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding: 10px;
+  color: blueviolet;
+}
+.center {
+  margin-top: 10rem;
+  text-align: center;
+}
+.center h1{
+  font-size: 30px;
+}
+.center p{
+  color: blueviolet;
+}
+.inputnumber {
+  margin-top: 100px;
+  margin-right: 20px;
+  margin-left: 20px;
+}
+.inputnumber p{
+  text-align: right;
+  color: $third_text_color;
+}
+.inputnumber div{
+   border: 2px solid rgb(245, 245, 245);
+  padding: 10px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  text-align: right;
+}
+.ghanin{
+  position: fixed;
+  bottom: 70px;
+  right: 0;
+  left: 0;
+  text-align: center;
+  color: $third_text_color;
+}
+.button{
+  position: fixed;
+  bottom: 10px;
+  right: 0;
+  left: 0;
+  margin-right: 20px;
+  margin-left: 20px;
+  border: 2px solid blueviolet;
+  border-radius: 15px;
+  background-color: blueviolet;
+  color: white;
+  text-align: center;
+  padding: 10px;
+}
 </style>
